a8348200a7277a1fbe940b19e198bd5b038e4ee5
brodyns <118648344+brodyns@users.noreply.github.com>
2024-02-28 20:33:53 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+        while(head != null){
+            vals.add(head.val);
+            head = head.next;
+        }
+        int front = 0;
+        int end = vals.size() - 1;
+        while(front < end){
+            if(!vals.get(front).equals(vals.get(end))){
+                return false;
+            }
+            front++;
+            end--;
+        }
+        return true;
+    }
+}
